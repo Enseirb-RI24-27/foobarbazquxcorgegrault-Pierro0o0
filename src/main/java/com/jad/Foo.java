ffb69867dc89bb334b949f5d046ce7741fccebe8
Pierre <you@example.com>
2026-01-12 16:48:02 +0100

    again

--- src/main/java/com/jad/Foo.java
+++ src/main/java/com/jad/Foo.java
@@ -1,6 +1,11 @@
 package com.jad;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Foo {
-    private bar Bar;
+    private Bar bar;             // <-bar : Bar (1)
+    private List<Baz> bazs;      // <-bazs : Baz[] (*)
+    private Qux qux;             // <-qux : Qux (1)
 
 }
