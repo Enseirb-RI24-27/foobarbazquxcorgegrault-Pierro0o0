f722034dc4c10af90f5620347b91b7d4bb6d497b
Pierre <you@example.com>
2026-01-12 16:56:41 +0100

    GetterSetter

--- src/main/java/com/jad/Foo.java
+++ src/main/java/com/jad/Foo.java
@@ -4,8 +4,34 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Foo {
+    public Bar getBar() {
+        return this.bar;
+    }
+
+    public List<Baz> getBazs() {
+        return this.bazs;
+    }
+
+    public Qux getQux() {
+        return this.qux;
+    }
+
+    public Corge getCorge() {
+        return this.corge;
+    }
+
+    public List<Grault> getGraults() {
+        return this.graults;
+    }
+
+    public void setCorge(Corge corge) {
+        this.corge = corge;
+    }
+
     private Bar bar;             // <-bar : Bar (1)
     private List<Baz> bazs;      // <-bazs : Baz[] (*)
     private Qux qux;             // <-qux : Qux (1)
+    private Corge corge;
+    private List<Grault> graults;
 
 }
