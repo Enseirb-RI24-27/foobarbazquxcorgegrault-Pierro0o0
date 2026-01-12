d5ec5005c7691f951be92a5b7b9b38fdb72bd1c3
Pierre <you@example.com>
2026-01-12 17:26:09 +0100

    fix

--- src/main/java/com/jad/Corge.java
+++ src/main/java/com/jad/Corge.java
@@ -2,13 +2,16 @@ package com.jad;
 
 public class Corge {
     private Foo foo;
+
     public Corge(Foo foo) {
-        this.foo =foo;
+        this.foo = foo;
     }
+
     public Foo getFoo() {
         return foo;
     }
 
     public void setFoo(Foo foo) {
         this.foo = foo;
+    }
 }
